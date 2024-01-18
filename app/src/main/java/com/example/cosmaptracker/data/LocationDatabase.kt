01d03b2b15da1740474d3d2275ec5cddb7fe9460
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-01-18 13:05:59 +0000

    feat: Database implemented (#3)

---
+++ app/src/main/java/com/example/cosmaptracker/data/LocationDatabase.kt
@@ -0,0 +1,42 @@
+package com.example.cosmaptracker.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Location::class], version = 1, exportSchema = false)
+abstract class LocationDatabase : RoomDatabase() {
+
+    /***
+     * Gets the location DAO.
+     */
+    abstract fun locationDao(): LocationDao
+
+    companion object {
+        /***
+         * Keeps a reference of the Database maintaining only one instance of the DB.
+         */
+        @Volatile
+        private var Instance: LocationDatabase? = null
+
+        /***
+         * Gets the database instance.
+         * @param context The context.
+         * @return The database instance.
+         */
+        fun getDatabase(context: Context): LocationDatabase {
+            // If the Instance is not null, return it, otherwise it creates the DB.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, LocationDatabase::class.java, "location_database")
+                    // If the schema changes we can add migration logic here.
+                    .fallbackToDestructiveMigration()
+                    // Building the database
+                    .build()
+                    // Keep the reference to the created DB.
+                    .also { Instance = it }
+            } // End of Synchronised
+        }
+
+    } // End of Companion Object
+} // End of Class
