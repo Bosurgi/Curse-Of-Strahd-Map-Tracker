ab22774420f5eb4adb9df8c4d6ffbe6226a90b7c
andrea.lafauci92@gmail.com <andrea.lafauci92@gmail.com>
2024-01-18 12:46:48 +0000

    feat: LocationDao and Location class implemented (#3)

--- app/src/main/java/com/example/cosmaptracker/data/Location.kt
+++ app/src/main/java/com/example/cosmaptracker/data/Location.kt
@@ -1,7 +1,20 @@
 package com.example.cosmaptracker.data
 
-data class Location(val name: String) {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-    var distanceTable: Map<Location, Int> = emptyMap()
+/***
+ * Data class for a location.
+ * @param id The id of the location.
+ * @param name The name of the location.
+ */
+@Entity(tableName = "locations")
+data class Location(
+    val name: String
+) {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+    // The distance table for this location to other locations.
+    val distanceTable: Map<Location, Int> = emptyMap()
 
 }
---
+++ app/src/main/java/com/example/cosmaptracker/data/LocationDao.kt
@@ -0,0 +1,47 @@
+package com.example.cosmaptracker.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface LocationDao {
+    /***
+     * Inserts a location into the database. If the location already exists, ignore the action.
+     * @param location The location to insert.
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertLocation(location: Location)
+
+    /***
+     * Updates a location in the database.
+     * @param location The location to update.
+     */
+    @Update
+    suspend fun updateLocation(location: Location)
+
+    /***
+     * Deletes a location from the database.
+     * @param location The location to delete.
+     */
+    @Delete
+    suspend fun deleteLocation(location: Location)
+
+    /***
+     * Gets all locations from the database.
+     */
+    @Query("SELECT * FROM locations ORDER by name ASC")
+     fun getAllLocations(): Flow<List<Location>>
+
+     /***
+      * Gets a location from the database.
+      * @param name The name of the location to get.
+      * @return The location with the given name.
+      */
+     @Query("SELECT * FROM locations WHERE name = :name")
+        fun getLocation(name: String): Flow<Location>
+}
